75af1e46022602ae758248931498045988957a93
phatnguyen <phatnguyen@gmx.de>
2024-11-21 15:47:51 +0100

    feat: input

--- src/input/input.ino
+++ src/input/input.ino
@@ -1,6 +1,6 @@
-const int d11 = 11; // Pin connected to W1 on 'dot' board
-const int d12 = 12; // Pin connected to W1 on 'dash' board
-const int baseDelay = 200; 
+const int d11 = 11;  // Pin connected to W1 on 'dot' board
+const int d12 = 12;  // Pin connected to W1 on 'dash' board
+const int baseDelay = 1000;
 const int dashDelay = baseDelay * 3;
 const int charDelay = baseDelay * 5;
 const int wordDelay = baseDelay * 25;
@@ -8,7 +8,6 @@ const int wordDelay = baseDelay * 25;
 void setup() {
   pinMode(d11, OUTPUT);
   pinMode(d12, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
   // Serial.begin(9600);
   // Serial.println("Input:");
 }
@@ -18,25 +17,23 @@ void loop() {
 
   // String input = Serial.readStringUntil('\n');
   // input.toUpperCase();
-  String input = "C"; //  "-.-."
-  
+  String input = "D";  //  "-.."
+
   for (int i = 0; i < input.length(); i++) {
     char c = input.charAt(i);
-    if(!isAlpha(c)) continue;
+    if (!isAlpha(c)) continue;
 
-    if(c == ' ') delay(wordDelay);
+    if (c == ' ') delay(wordDelay);
     else {
       encode(c);
-
-      if(i != input.length() -1)
-        delay(charDelay);
+      delay(charDelay);
     }
   }
 
   // Serial.println("End!");
 }
 
-void encode(char c){
+void encode(char c) {
   String code = getMorseCode(c);
   for (int i = 0; i < code.length(); i++) {
     if (code[i] == '.') {
@@ -75,16 +72,6 @@ String getMorseCode(char c) {
     case 'X': return "-..-";
     case 'Y': return "-.--";
     case 'Z': return "--..";
-    case '1': return ".----";
-    case '2': return "..---";
-    case '3': return "...--";
-    case '4': return "....-";
-    case '5': return ".....";
-    case '6': return "-....";
-    case '7': return "--...";
-    case '8': return "---..";
-    case '9': return "----.";
-    case '0': return "-----";
     default: return "";
   }
 }
@@ -92,19 +79,14 @@ String getMorseCode(char c) {
 
 void sendDot() {
   digitalWrite(d11, HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);
   delay(baseDelay);
   digitalWrite(d11, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
   delay(baseDelay);
 }
 
 void sendDash() {
   digitalWrite(d12, HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);
   delay(dashDelay);
   digitalWrite(d12, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
   delay(baseDelay);
 }
-
